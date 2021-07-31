42c3588588b0bb0531510df237e18a3d0f590406
Rizki Tirta Buwono <rizkibuwono0@gmail.com>
2021-07-31 13:38:25 +0700

    Basic widget

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Hello"),
+        ),
+        body: Center(child: Text("Hello body")),
+      ),
+    );
+  }
+}
