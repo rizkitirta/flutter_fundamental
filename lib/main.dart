403fcb84f8dd3166c7b74a49ff5bff7d5f14f981
Rizki Tirta Buwono <rizkibuwono0@gmail.com>
2021-08-01 21:07:14 +0700

    Use Text Field

--- lib/main.dart
+++ lib/main.dart
@@ -5,76 +5,48 @@ void main(List<String> args) {
   runApp(myApp());
 }
 
-class myApp extends StatelessWidget {
+class myApp extends StatefulWidget {
+  @override
+  _myAppState createState() => _myAppState();
+}
+
+class _myAppState extends State<myApp> {
+  final TextEditingController myController = TextEditingController();
+
+  String hasil = "Hasil Input";
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Fitur Text Field"),
+          title: Text("Use Text Field"),
         ),
         body: Center(
-          child: Padding(
-              padding: const EdgeInsets.all(20),
-              child: Column(
-                children: [
-                  TextField(
-                    showCursor: true,
-                    //cursorColor: Colors.yellow,
-                    // cursorWidth: 5,
-                    // cursorHeight: 25,
-                    // cursorRadius: Radius.circular(20),
-
-                    textAlign: TextAlign.start,
-                    textCapitalization: TextCapitalization.none,
-
-                    //style: TextStyle(color: Colors.red, fontSize: 20),
-
-                    decoration: InputDecoration(
-                        icon: Icon(
-                          Icons.person,
-                          size: 35,
-                        ),
-                        border: OutlineInputBorder(),
-                        // prefixIcon: Icon(
-                        //   Icons.person_add,
-                        //   size: 35,
-                        // ),
-                        //prefixText: "Name :",
-                        hintText: "Please input in here..",
-                        labelText: "Username"),
-                  ),
-                  SizedBox(
-                    height: 15,
-                  ),
-                  TextField(
-                    showCursor: true,
-                    textAlign: TextAlign.start,
-                    textCapitalization: TextCapitalization.none,
-                    obscureText: true,
-
-                    decoration: InputDecoration(
-                      icon: Icon(
-                        Icons.lock,
-                        size: 35,
-                      ),
-                      border: OutlineInputBorder(),
-                      // prefixIcon: Icon(
-                      //   Icons.person_add,
-                      //   size: 35,
-                      // ),
-                      //prefixText: "Name :",
-                      hintText: "Please input in here..",
-                      labelText: "Password",
-                      suffixIcon: IconButton(
-                        icon: Icon(Icons.remove_red_eye),
-                        onPressed: () {} ,
-                      )
-                    ),
-                  ),
-                ],
-              )),
-        ),
+            child: Padding(
+          padding: const EdgeInsets.all(25.0),
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                TextField(
+                  controller: myController,
+                  onChanged: (value) {
+                    print("Onchanged");
+                  },
+                  onSubmitted: (value) {
+                    print(value);
+                    setState(() {
+                      hasil = value;
+                    });
+                  },
+                  onEditingComplete: () {
+                    print("Edited Success");
+                    print(myController.text);
+                  } ,
+                ),
+                Text(hasil)
+              ]),
+        )),
       ),
     );
   }
