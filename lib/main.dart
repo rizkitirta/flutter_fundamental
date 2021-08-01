0f680d9b7aa443f6891b33f0d072daffbfc158cc
Rizki Tirta Buwono <rizkibuwono0@gmail.com>
2021-08-01 14:03:55 +0700

    Tab Bar Widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,40 +1,66 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 
 void main(List<String> args) {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  List<Tab> myTabs = [
+    Tab(
+      text: "Tab 1",
+      icon: Icon(Icons.add_a_photo),
+    ),
+    Tab(
+      icon: Icon(Icons.ac_unit_rounded),
+    ),
+    Tab(
+      text: "Tab 3",
+    ),
+    Tab(
+      icon: Icon(Icons.file_copy),
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          leading: Container(
-            color: Colors.greenAccent,
-          ),
-          title: Text("App Bar"),
-          centerTitle: false,
-          actions: [
-            Container(
-              width: 50,
-              color: Colors.yellow,
+      home: DefaultTabController(
+        initialIndex: 1,
+        length: myTabs.length,
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text("My App"),
+            bottom: TabBar(
+              labelColor: Colors.black,
+              unselectedLabelColor: Colors.white,
+              indicatorColor: Colors.purple,
+              indicatorWeight: 5,
+              //custom indicator
+              indicator: BoxDecoration(
+                  color: Colors.greenAccent.withOpacity(0.2),
+                  //borderRadius: BorderRadius.circular(50),
+                  border: Border(
+                      bottom: BorderSide(color: Colors.greenAccent, width: 5))),
+              tabs: myTabs,
             ),
-          ],
-          bottom: PreferredSize(
-            preferredSize: Size.fromHeight(60),
-            child: Container(
-              width: 30,
-              height: 30,
-              color: Colors.red,
-            ),
           ),
-          flexibleSpace: Container(
-            height: 30,
-            color: Colors.orange,
+          body: TabBarView(
+            children: [
+              Center(
+                child: Text("Page Tab 1"),
+              ),
+              Center(
+                child: Text("Page Tab 2"),
+              ),
+              Center(
+                child: Text("Page Tab 3"),
+              ),
+              Center(
+                child: Text("Page Tab 4"),
+              ),
+            ],
           ),
         ),
       ),
