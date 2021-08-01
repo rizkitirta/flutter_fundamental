83206a97d2c2ec117d70c9a9d6d85092166f5e42
Rizki Tirta Buwono <rizkibuwono0@gmail.com>
2021-08-01 11:56:39 +0700

    Mapping List

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:faker/faker.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -6,51 +5,114 @@ void main(List<String> args) {
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  int counter = 1;
+class MyApp extends StatelessWidget {
+  //Fake Data
+  final List<Map<String, dynamic>> myList = [
+    {
+      "name": "Rizki Tirta",
+      "age": 19,
+      "favColors": [
+        "blue",
+        "white",
+        "red",
+        "blue",
+        "white",
+        "red",
+        "blue",
+        "white",
+        "red",
+        "red",
+        "blue",
+        "white",
+        "red"
+      ]
+    },
+    {
+      "name": "M. Arif Fadilah",
+      "age": 16,
+      "favColors": ["white", "blue", "red", "red", "blue", "white", "red"]
+    },
+    {
+      "name": "Meilin Rizkiana",
+      "age": 24,
+      "favColors": ["blue", "white", "red", "red", "blue", "white", "red"]
+    },
+    {
+      "name": "Nurma Rindiasti",
+      "age": 26,
+      "favColors": ["blue", "white", "red", "red", "blue", "white", "red"]
+    },
+    {
+      "name": "Tirta",
+      "age": 19,
+      "favColors": ["blue", "white", "red", "red", "blue", "white", "red"]
+    },
+    {
+      "name": "Nurma Rindiasti",
+      "age": 26,
+      "favColors": ["blue", "white", "red", "red", "blue", "white", "red"]
+    },
+    {
+      "name": "Tirta",
+      "age": 19,
+      "favColors": ["blue", "white", "red", "red", "blue", "white", "red"]
+    }
+  ];
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Chat Apps"),
+          title: Text("Mapping List"),
         ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Text(
-              counter.toString(),
-              style: TextStyle(fontSize: 50 + double.parse(counter.toString())),
-            ),
-            Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
-              RaisedButton(
-                onPressed: () {
-                  if (counter != 1) {
-                    setState(() {
-                      counter--;
-                    });
-                  }
-                  print(counter);
-                },
-                child: Icon(Icons.remove),
+        body: ListView(
+          children: myList.map((data) {
+            List favColors = data['favColors'];
+
+            return Card(
+              margin: EdgeInsets.all(15),
+              child: Container(
+                margin: EdgeInsets.all(15),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      children: [
+                        CircleAvatar(),
+                        SizedBox(
+                          width: 15,
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text("Name: ${data['name']}"),
+                            Text("Age: ${data['age']}"),
+                          ],
+                        )
+                      ],
+                    ),
+                    SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      child: Row(
+                        children: favColors.map((color) {
+                          return Container(
+                            color: Colors.black.withOpacity(0.1),
+                            margin: EdgeInsets.symmetric(
+                              horizontal: 10,
+                              vertical: 10,
+                            ),
+                            padding: EdgeInsets.all(5),
+                            child: Text(color),
+                          );
+                        }).toList(),
+                      ),
+                    )
+                  ],
+                ),
               ),
-              RaisedButton(
-                onPressed: () {
-                  setState(() {
-                    counter++;
-                  });
-                  print(counter);
-                },
-                child: Icon(Icons.add),
-              ),
-            ])
-          ],
+            );
+          }).toList(),
         ),
       ),
     );
