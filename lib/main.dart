a3eca082b51fb848bf9449a65ddbf4389f4c1ded
Rizki Tirta Buwono <rizkibuwono0@gmail.com>
2021-08-01 14:20:01 +0700

    Fitur text field flutter

--- lib/main.dart
+++ lib/main.dart
@@ -2,66 +2,32 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main(List<String> args) {
-  runApp(MyApp());
+  runApp(myApp());
 }
 
-class MyApp extends StatelessWidget {
-  List<Tab> myTabs = [
-    Tab(
-      text: "Tab 1",
-      icon: Icon(Icons.add_a_photo),
-    ),
-    Tab(
-      icon: Icon(Icons.ac_unit_rounded),
-    ),
-    Tab(
-      text: "Tab 3",
-    ),
-    Tab(
-      icon: Icon(Icons.file_copy),
-    ),
-  ];
-
+class myApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: DefaultTabController(
-        initialIndex: 1,
-        length: myTabs.length,
-        child: Scaffold(
-          appBar: AppBar(
-            title: Text("My App"),
-            bottom: TabBar(
-              labelColor: Colors.black,
-              unselectedLabelColor: Colors.white,
-              indicatorColor: Colors.purple,
-              indicatorWeight: 5,
-              //custom indicator
-              indicator: BoxDecoration(
-                  color: Colors.greenAccent.withOpacity(0.2),
-                  //borderRadius: BorderRadius.circular(50),
-                  border: Border(
-                      bottom: BorderSide(color: Colors.greenAccent, width: 5))),
-              tabs: myTabs,
-            ),
-          ),
-          body: TabBarView(
-            children: [
-              Center(
-                child: Text("Page Tab 1"),
-              ),
-              Center(
-                child: Text("Page Tab 2"),
-              ),
-              Center(
-                child: Text("Page Tab 3"),
-              ),
-              Center(
-                child: Text("Page Tab 4"),
-              ),
-            ],
-          ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Fitur Text Field"),
+        ),
+        body: Center(
+          child: Padding(
+              padding: const EdgeInsets.all(20),
+              child: Column(
+                children: [
+                  TextField(
+                    autocorrect: false,
+                    keyboardType: TextInputType.number,
+                  ),
+                  TextField(
+                    autocorrect: false,
+                    //autofocus: true,
+                  ),
+                ],
+              )),
         ),
       ),
     );
