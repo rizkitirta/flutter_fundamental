3f225aa0fddaf30936d95bbaefc1c0b2d2cc0bcc
Rizki Tirta Buwono <rizkibuwono0@gmail.com>
2021-08-02 22:34:08 +0700

    Drawer | Sidebar

--- lib/main.dart
+++ lib/main.dart
@@ -12,13 +12,69 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: PageSatu(),
-      initialRoute: PageSatu.routeName,
-      routes: {
-        PageSatu.routeName: (context) => PageSatu(),
-        PageDua.routeName: (context) => PageDua(),
-        PageTiga.routeName: (context) => PageTiga(),
-      },
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        //leading: Icon(Icons.menu),
+        title: Text("Home Drawer"),
+      ),
+      drawer: Drawer(
+        child: Column(children: [
+          Container(
+            padding: EdgeInsets.all(25),
+            width: double.infinity,
+            height: 120,
+            color: Colors.blueAccent,
+            alignment: Alignment.bottomLeft,
+            child: Text(
+              "Menu Drawer",
+              style: TextStyle(fontSize: 25, color: Colors.white),
+            ),
+          ),
+          SizedBox(height: 15),
+          ListTile(
+            onTap: () => Navigator.of(context).pushReplacement(
+                MaterialPageRoute(builder: (context) => HomePage())),
+            leading: Icon(
+              Icons.home,
+              size: 25,
+            ),
+            title: Text(
+              "Home",
+              style: TextStyle(fontSize: 20),
+            ),
+          ),
+          ListTile(
+            onTap: () {
+              Navigator.of(context)
+                  .pushReplacement(MaterialPageRoute(builder: (context) {
+                return PageSatu();
+              }));
+            },
+            leading: Icon(
+              Icons.settings,
+              size: 25,
+            ),
+            title: Text(
+              "Settings",
+              style: TextStyle(fontSize: 20),
+            ),
+          ),
+        ]),
+      ),
+      body: Center(
+        child: Text(
+          "Some text here..",
+          style: TextStyle(fontSize: 35),
+        ),
+      ),
     );
   }
 }
--- lib/pages/page_1.dart
+++ lib/pages/page_1.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/main.dart';
 import 'package:flutter_application_1/pages/page_2.dart';
 
 class PageSatu extends StatelessWidget {
@@ -8,7 +9,51 @@ class PageSatu extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Page 1"),
+        title: Text("Settings"),
+      ),
+      drawer: Drawer(
+        child: Column(children: [
+          Container(
+            padding: EdgeInsets.all(25),
+            width: double.infinity,
+            height: 120,
+            color: Colors.blueAccent,
+            alignment: Alignment.bottomLeft,
+            child: Text(
+              "Menu Drawer",
+              style: TextStyle(fontSize: 25, color: Colors.white),
+            ),
+          ),
+          SizedBox(height: 15),
+          ListTile(
+            onTap: () => Navigator.of(context).pushReplacement(
+                MaterialPageRoute(builder: (context) => HomePage())),
+            leading: Icon(
+              Icons.home,
+              size: 25,
+            ),
+            title: Text(
+              "Home",
+              style: TextStyle(fontSize: 20),
+            ),
+          ),
+          ListTile(
+            onTap: () {
+              Navigator.of(context)
+                  .pushReplacement(MaterialPageRoute(builder: (context) {
+                return PageDua();
+              }));
+            },
+            leading: Icon(
+              Icons.settings,
+              size: 25,
+            ),
+            title: Text(
+              "Settings",
+              style: TextStyle(fontSize: 20),
+            ),
+          ),
+        ]),
       ),
       body: Center(
         child: Text(
