34cd15e192fc42841109278104d94ffcc4509370
Rizki Tirta Buwono <rizkibuwono0@gmail.com>
2021-08-04 21:15:30 +0700

    Flexible & Expanded

--- lib/pages/homePage.dart
+++ lib/pages/homePage.dart
@@ -1,82 +1,49 @@
-import 'dart:math';
-
-import 'package:faker/faker.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/model/Product.dart';
+import 'package:flutter_application_1/main.dart';
 
-class HomePage extends StatelessWidget {
+void main(List<String> args) {
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
-    final mediaQueryHeight = MediaQuery.of(context).size.height;
-    final mediaQueryWidth = MediaQuery.of(context).size.width;
-    final appBar = AppBar(
-      title: Text("Home Page"),
+    return MaterialApp(
+      home: HomePage(),
     );
+  }
+}
 
-    final bodyHeight = mediaQueryHeight -
-        appBar.preferredSize.height -
-        MediaQuery.of(context).padding.top;
+class HomePage extends StatelessWidget {
 
-    final bool IsLandscape =
-        MediaQuery.of(context).orientation == Orientation.landscape;
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
-      appBar: appBar,
-      body: Center(
-        child: (IsLandscape)
-            ? Column(
-                children: [
-                  Container(
-                    height: bodyHeight * 0.5,
-                    width: mediaQueryWidth,
-                    color: Colors.orange,
-                  ),
-                  Container(
-                    height: bodyHeight * 0.5,
-                    color: Colors.grey,
-                    child: GridView.builder(
-                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                          crossAxisCount: 2,
-                          mainAxisSpacing: 5,
-                          crossAxisSpacing: 5),
-                      itemBuilder: (context, index) {
-                        return GridTile(
-                          child: Container(
-                            color: Color.fromARGB(255, Random().nextInt(156),
-                                Random().nextInt(156), Random().nextInt(156)),
-                          ),
-                        );
-                      },
-                      itemCount: 100,
-                    ),
-                  ),
-                ],
-              )
-            : Column(
-                children: [
-                  Container(
-                    height: bodyHeight * 0.3,
-                    width: 400,
-                    color: Colors.orange,
-                  ),
-                  Container(
-                    height: bodyHeight * 0.7,
-                    color: Colors.grey,
-                    child: ListView.builder(
-                      itemBuilder: (context, index) {
-                        return ListTile(
-                          leading: CircleAvatar(),
-                          title: Text("Hallo semua.."),
-                        );
-                      },
-                      itemCount: 100,
-                    ),
-                  ),
-                ],
-              ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add_a_photo),
-        onPressed: null,
+      appBar: AppBar(title: Text("Flexible & Expanded"),),
+      body: Row(
+        //mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            height: 100,
+            color: Colors.green,
+            child: Text("Teks 1 panjang...."),
+          ),
+          Container(
+            height: 100,
+            color: Colors.amber,
+            child: Text("Teks 2 sedang"),
+          ),
+          Flexible(
+            fit: FlexFit.tight,
+            child: Container(
+              height: 100,
+              color: Colors.blue,
+              child: Text("Teks 3"),
+            ),
+          )
+        ],
       ),
     );
   }
