2a51bbac08cd373dd2a56dbf8de8d4f488ca1ef1
Rizki Tirta Buwono <rizkibuwono0@gmail.com>
2021-08-07 14:52:24 +0700

    Consumer + Provider

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/providers/product.dart';
 import 'package:provider/provider.dart';
@@ -8,7 +7,7 @@ import '../screens/product_detail_screen.dart';
 class ProductItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final productData = Provider.of<Product>(context);
+    final productData = Provider.of<Product>(context, listen: false);
 
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
@@ -27,14 +26,16 @@ class ProductItem extends StatelessWidget {
         ),
         footer: GridTileBar(
           backgroundColor: Colors.black87,
-          leading: IconButton(
-            icon: (productData.isFavorite)
-                ? Icon(Icons.favorite)
-                : Icon(Icons.favorite_border_outlined),
-            color: Theme.of(context).accentColor,
-            onPressed: () {
-             return productData.statusFavorit();
-            },
+          leading: Consumer<Product>(
+            builder: (context, productData, child) => IconButton(
+              icon: (productData.isFavorite)
+                  ? Icon(Icons.favorite)
+                  : Icon(Icons.favorite_border_outlined),
+              color: Theme.of(context).accentColor,
+              onPressed: () {
+                return productData.statusFavorit();
+              },
+            ),
           ),
           title: Text(
             productData.title,
